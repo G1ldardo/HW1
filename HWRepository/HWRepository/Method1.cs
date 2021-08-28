6ff1c1b9f3d5be8a62cdf6a60b1c18b94ccf1452
G1ldardo <gil.glz.29@gmail.com>
2021-08-28 10:42:16 +0200

    Creation of Method1

---
+++ HWRepository/HWRepository/Method1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HWRepository
+{
+    class Method1
+    {
+        public void PrintMe()
+        {
+            Console.WriteLine("I'm method 1");
+        }
+    }
+}
---
+++ HWRepository/HWRepository/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HWRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git World! Oops I changed again!");
+            Method1 m1 = new Method1();
+            m1.PrintMe();
+        }
+    }
+}
